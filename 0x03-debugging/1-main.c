a035dd56b479cf0a180ef270e111f9997481026c
FaradayJnr <faradayfoundationgh@gmail.com>
2022-12-15 18:01:06 -0800

    more loops

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,18 +6,19 @@
  */
 int main(void)
 {
-      	int i;
+int i;
 
-        printf("Infinite loop incoming :(\n");
- 
- 	i = 0;
+printf("Infinite loop incoming :(\n");
 
-        /**
-	 * while (i < 10)
- 	 * { 
-   	 *	putchar(i);
-	 * }
-         */
-	printf("Infinite loop avoided! \\o/\n");
-	return (0);
+i = 0;
+
+/**
+ * while (i < 10)
+ * { 
+ *	putchar(i);
+ * }
+ */
+
+printf("Infinite loop avoided! \\o/\n");
+return (0);
 }
